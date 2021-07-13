c71fae7ce988b3488bc995b3cf4d0f7f19cf8c80
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-07-13 16:57:17 +1000

    Insert into database

---
+++ db/db.js
@@ -0,0 +1,63 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'products',
+});
+
+function insertProducts(products) {
+    connection.connect();
+    for (const product of products) {
+        connection.query(
+            `
+            INSERT INTO items
+                (
+                    name,
+                    origin_id,
+                    price,
+                    quantity,
+                    fk_unit,
+                    fk_category
+                )
+                VALUES
+                (
+                    ?,
+                    (
+                        SELECT id
+                        FROM origin
+                        WHERE name = ?
+                    ),
+                    ?,
+                    ?,
+                    (
+                        SELECT id
+                        FROM unit
+                        WHERE name = ?
+                    ),
+                    (
+                        SELECT id
+                        FROM category
+                        WHERE name = ?
+                    )
+                );
+            `,
+            [
+                product.name,
+                product.origin,
+                product.price,
+                product.qty,
+                product.unit,
+                product.category,
+            ],
+            (err, result) => {
+                if (err) throw err;
+        })
+    }
+    connection.end();
+}
+
+module.exports = {
+    insertProducts: insertProducts,
+};
