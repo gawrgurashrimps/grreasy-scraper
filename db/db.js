0adb71b6ba75287f54712cac939b7bd8fa0b7b5a
Mariusz Skoneczko <mariusz@skoneczko.com>
2021-07-14 16:16:00 +1000

    Create connection pool

--- db/db.js
+++ db/db.js
@@ -1,15 +1,34 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'products',
+    connectionLimit: 10,
+    supportBigNumbers: true
 });
+
+function query(sql, args) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                return reject(err);
+            }
+            connection.query(sql, args, function(err, result) {
+                connection.release();
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result);
+            });
+        });
+    });
+}
 
 function insertProducts(products) {
     for (const product of products) {
-        connection.query(
+        query(
             `
             INSERT INTO items
                 (
@@ -49,10 +68,8 @@ function insertProducts(products) {
                 product.qty,
                 product.unit,
                 product.category,
-            ],
-            (err, result) => {
-                if (err) throw err;
-        })
+            ]
+        )
     }
 }
 
